779aa97bd1abc42ff517e1b6e8676c41ab6dc264
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2022-02-01 16:15:40 +0300

    Add files and write logic equals

---
+++ src/main/java/Test_Stagirovka/Equals_and_Ravno_Ravno.java
@@ -0,0 +1,9 @@
+package Test_Stagirovka;
+
+public class Equals_and_Ravno_Ravno {
+    public static void main(String[] args){
+        Double array[] = {3453d, 1224523d, 999000d};
+        Double first = 3453d;
+        System.out.println(array[0].equals(first));
+    }
+}
